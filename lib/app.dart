3048aac95a189c9dc4be7e9c8ed1606b75e51854
Alejandro Nuñez <nunezg.john@gmail.com>
2023-10-20 15:27:48 -0500

    add: base configuration and barrels files

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MovieEvertecApp extends StatelessWidget {
+  const MovieEvertecApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Movie Evertec App',
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
